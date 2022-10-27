a993af4bdba408ac01120962e9e6e04855eca8b8
jaysmith13 <janeasmith13@gmail.com>
2022-10-26 18:07:17 -0600

    Update mean median mode.py

--- mean median mode.py
+++ mean median mode.py
@@ -6,7 +6,7 @@ list1 = [12, 16, 20, 12, 30, 25, 23, 24, 20]
 mean = sum(list1)/len(list1)
 print(mean)
 
-## Median
+# Median
 list1 = [12, 16, 20, 12, 30, 25, 23, 24, 20]
 list1.sort()
 
@@ -17,3 +17,15 @@ else:
     median = list1[len(list1)//2]
 print(median)
 
+# Mode
+list1 = [12, 16, 20, 20, 12, 30, 25, 23, 24, 20]
+frequency = {}
+for i in list1:
+    frequency.setdefault(i, 0)
+    frequency[i]+=1
+
+frequent = max(frequency.values())
+for i, j in frequency.items():
+    if j == frequent:
+        mode = i
+print(mode)
